a7df2fc9f51b7adcc430fac052636892be7ac5be
Shahidafridi3303 <115124671+Shahidafridi3303@users.noreply.github.com>
2024-03-20 02:42:24 -0400

    .

---
+++ Assignment 2/Unity Project/Assets/Scripts/Bumper.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    // Adjust this force value to control the strength of the push
+    public float m_fForceModifier = 10f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the colliding object is the ball
+        if (collision.gameObject.tag == "Ball")
+        {
+            float force = -collision.gameObject.GetComponent<Rigidbody>().angularVelocity.magnitude * m_fForceModifier;
+            Vector3 contactPosition = collision.GetContact(0).point;
+            Vector3 direction = collision.gameObject.transform.position - contactPosition;
+            collision.gameObject.GetComponent<Rigidbody>().AddForceAtPosition(direction * force, contactPosition, ForceMode.Impulse);
+        }
+    }
+}

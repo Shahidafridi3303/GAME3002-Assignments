3b7124bc901756b6a060e2e76ed44c63676d7647
Shahidafridi3303 <115124671+Shahidafridi3303@users.noreply.github.com>
2024-03-24 10:26:49 -0400

    Impressive

--- Assignment 2/Unity Project/Assets/Scripts/Ball.cs
+++ Assignment 2/Unity Project/Assets/Scripts/Ball.cs
@@ -4,29 +4,38 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter(Collision collision)
     {
         string tag = collision.gameObject.tag;
         switch (tag)
         {
             case "Dead":
                 GameManager.instance.GameEnd();
+                print("Dead");
+
                 break;
 
             case "Bouncer":
                 GameManager.instance.UpdateScore(10, 1);
+                print("bouncer");
                 break;
 
             case "Point":
                 GameManager.instance.UpdateScore(20, 1);
+                print("Point");
+
                 break;
 
             case "Side":
                 GameManager.instance.UpdateScore(10, 0);
+                print("Side");
+
                 break;
 
             case "Flipper":
                 GameManager.instance.multiplier = 1;
+                print("Flipper");
+
                 break;
 
             default:

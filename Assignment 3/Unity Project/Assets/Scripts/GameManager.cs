683857bf6f8e312c79276166fd4178e776334aa2
Shahid Afridi <115124671+Shahidafridi3303@users.noreply.github.com>
2024-04-13 14:42:55 -0400

    1

--- Assignment 3/Unity Project/Assets/Scripts/GameManager.cs
+++ Assignment 3/Unity Project/Assets/Scripts/GameManager.cs
@@ -6,10 +6,16 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    // Timer
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+    private bool startTimer = false;
+
+    // Other UI elements
     [SerializeField]
     GameObject startButton, keyText,
     quitButton, restartButton, BGImage, Instructions;
-
+    
     public static GameManager instance;
     int key = 0;
 
@@ -27,6 +33,14 @@ public class GameManager : MonoBehaviour
         Instructions.SetActive(true);
     }
 
+    void Update()
+    {
+        if (startTimer)
+        {
+            StartCountdownTimer();
+        }
+    }
+
     public void UpdateKeyScore()
     {
         key += 1;
@@ -41,6 +55,7 @@ public class GameManager : MonoBehaviour
 
     public void GameStart()
     {
+        startTimer = true;
         startButton.SetActive(false);
         Instructions.SetActive(false);
         BGImage.SetActive(false);
@@ -58,5 +73,27 @@ public class GameManager : MonoBehaviour
     public void GameRestart()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    // Timer
+    void StartCountdownTimer()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+            timerText.color = Color.red;
+
+            Time.timeScale = 0;
+            GameEnd();
+        }
+
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
